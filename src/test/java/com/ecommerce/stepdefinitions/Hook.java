dff2a6b4208f620f2a8d9205d1f3f1d958db64ef
Mehmet Gul <mehmet@clarusway.com>
2023-03-21 20:08:17 -0400

    Hook class update for scenario name and tag print

--- src/test/java/com/ecommerce/stepdefinitions/Hook.java
+++ src/test/java/com/ecommerce/stepdefinitions/Hook.java
@@ -3,19 +3,25 @@ package com.ecommerce.stepdefinitions;
 import com.ecommerce.utility.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 
 import java.time.Duration;
 
 public class Hook {
     @Before
-    public void setup(){
+    public void setup(Scenario scenario){
+        System.out.println("Starting scenario: " + scenario.getName());
+        System.out.println("Tags: " + scenario.getSourceTagNames());
         Driver.getDriver().manage().window().maximize();
         Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario){
+        System.out.println("Ending scenario: " + scenario.getName());
         Driver.close();
     }
 }
